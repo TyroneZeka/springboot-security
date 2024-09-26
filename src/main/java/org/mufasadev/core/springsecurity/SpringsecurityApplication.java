2c9a45b682cb57d8c82c866741054f63013713b9
Tyrone Zeka <mufasadev@zohomail.com>
2024-09-26 11:27:25 +0800

    init commit

---
+++ src/main/java/org/mufasadev/core/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,13 @@
+package org.mufasadev.core.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/mufasadev/core/springsecurity/controllers/GreetingsController.java
@@ -0,0 +1,13 @@
+package org.mufasadev.core.springsecurity.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> greeting() {
+        return ResponseEntity.ok("Hello " + "World" + "!");
+    }
+}
