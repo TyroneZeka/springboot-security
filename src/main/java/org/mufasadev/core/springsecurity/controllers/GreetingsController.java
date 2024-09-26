6c515559af01aff04e613ddca26521618cac51c2
Tyrone Zeka <mufasadev@zohomail.com>
2024-09-26 14:03:35 +0800

    Hashing

--- src/main/java/org/mufasadev/core/springsecurity/controllers/GreetingsController.java
+++ src/main/java/org/mufasadev/core/springsecurity/controllers/GreetingsController.java
@@ -1,6 +1,7 @@
 package org.mufasadev.core.springsecurity.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingsController {
     @GetMapping("/hello")
     public ResponseEntity<String> greeting() {
         return ResponseEntity.ok("Hello " + "World" + "!");
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> adminEndpoint() {
+        return ResponseEntity.ok("Hello " + "Admin" + "!");
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<String> userEndpoint() {
+        return ResponseEntity.ok("Hello " + "User" + "!");
     }
 }
--- src/main/java/org/mufasadev/core/springsecurity/security/SecurityConfig.java
+++ src/main/java/org/mufasadev/core/springsecurity/security/SecurityConfig.java
@@ -1,22 +1,69 @@
 package org.mufasadev.core.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
+
     @Bean
-    SecurityFilterChain ftC (HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+    SecurityFilterChain defaultSecurityFilterChain (HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.
+                requestMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated());
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
+        http.csrf(csrf-> csrf.disable());
+        http.headers(
+                headers-> headers.frameOptions(
+                        frameOptions-> frameOptions.sameOrigin()
+                ));
         return http.build();
+    }
+
+    /* Using InMemory user for authentication, the users are not persisting in the Database */
+    @Bean
+    public UserDetailsService userDetailsService () {
+        UserDetails user1 = User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user1);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
+        // return new InMemoryUserDetailsManager(user1,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
